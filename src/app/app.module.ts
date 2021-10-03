1ec775b33dab097c4648ad40dfcd964499f0643b
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-10-03 08:36:08 +0200

    add pwa support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { BrowserModule } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { environment } from '../environments/environment';
 
@@ -25,7 +26,13 @@ import { LoadingComponent } from './components/loading/loading.component';
     AngularFireModule.initializeApp(environment.firebaseConfig, 'ordersystem'),
     AngularFireAuthModule,
     AngularFireDatabaseModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [ AppComponent ]
