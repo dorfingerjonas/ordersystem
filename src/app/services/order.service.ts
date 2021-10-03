f6c724650e098df1950cbc71ddae3fd2c374cf67
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-10-03 09:15:54 +0200

    save orders to stats

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -11,6 +11,8 @@ export class OrderService {
   }
 
   persist(order: Order): Promise<void> {
+    this.db.list('stats/orders').push(order);
+
     return new Promise((resolve, reject) => {
       this.db.list('orders').push(order)
         .then(res => resolve())
