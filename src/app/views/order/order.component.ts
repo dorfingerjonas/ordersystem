6ac48341569481600843f7ec7933b0a76cfee9d3
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-10-03 06:47:04 +0200

    implement function to submit order

--- src/app/views/order/order.component.ts
+++ src/app/views/order/order.component.ts
@@ -1,58 +1,96 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { faMinus as minusIcon } from '@fortawesome/free-solid-svg-icons';
-import { Drink, Food } from '../../models/models';
+import { Subscription } from 'rxjs';
+import { Drink, Food, Order } from '../../models/models';
+import { AuthService } from '../../services/auth.service';
 import { DrinkService } from '../../services/drink.service';
 import { FoodService } from '../../services/food.service';
 import { HeaderService } from '../../services/header.service';
 import { LoadingService } from '../../services/loading.service';
+import { OrderService } from '../../services/order.service';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: [ './order.component.scss' ]
 })
-export class OrderComponent implements OnInit {
+export class OrderComponent implements OnInit, OnDestroy {
 
   displayTable: { title: string; items: Drink[] | Food[] } [];
   minusIcon = minusIcon;
+  tableNr: number;
   private drinks: Drink[];
   private food: Food[];
+  private subscriptions: Subscription[] = [];
 
   constructor(private router: Router,
               private loading: LoadingService,
               private header: HeaderService,
               private drinkService: DrinkService,
-              private foodService: FoodService) {
+              private foodService: FoodService,
+              private orderService: OrderService,
+              private auth: AuthService) {
     this.displayTable = [];
     this.drinks = [];
     this.food = [];
+    this.tableNr = parseInt(this.router.url.replace('/order/', ''), 0);
   }
 
   ngOnInit(): void {
-    setTimeout(() => this.header.text = `Tisch: ${ this.router.url.replace('/order/', '') }`);
+    setTimeout(() => this.header.text = `Tisch: ${ this.tableNr }`);
 
-    this.drinkService.drinks.subscribe(drinks => {
+    let sub = this.drinkService.drinks.subscribe(drinks => {
       this.drinks = drinks;
 
       this.buildDisplayTable();
     });
 
-    this.foodService.food.subscribe(food => {
+    this.subscriptions.push(sub);
+
+    sub = this.foodService.food.subscribe(food => {
       this.food = food;
 
       this.buildDisplayTable();
+    });
+
+    this.subscriptions.push(sub);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(value => {
+      value.unsubscribe();
     });
   }
 
   decreaseAmount(item: Drink | Food): void {
     if (item.amount) {
       if (item.amount > 0) {
-        console.log(item.amount);
         item.amount = item.amount - 2;
-        console.log(item.amount);
       }
     }
+  }
+
+  order(): void {
+    const order: Order = {
+      drinks: this.drinks.filter(drink => (drink.amount || 0) > 0),
+      food: this.food.filter(food => (food.amount || 0) > 0),
+      table: {
+        nr: this.tableNr
+      },
+      timestamp: Date.now(),
+      waiter: this.auth.username
+    };
+
+    this.loading.activateLoading();
+
+    this.orderService.persist(order).then(value => {
+      this.loading.deactivateLoading();
+
+      this.router.navigateByUrl('/tables');
+    }).catch(reason => {
+      this.loading.deactivateLoading();
+    });
   }
 
   private buildDisplayTable(): void {
@@ -72,5 +110,12 @@ export class OrderComponent implements OnInit {
     ];
 
     this.loading.deactivateLoading();
+  }
+
+  isOrderValid(): boolean {
+    const drinks = this.drinks.filter(drink => (drink.amount || 0) > 0);
+    const food = this.food.filter(f => (f.amount || 0) > 0);
+
+    return drinks.length > 0 || food.length > 0;
   }
 }
