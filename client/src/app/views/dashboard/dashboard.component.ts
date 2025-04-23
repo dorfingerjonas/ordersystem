0643843963ea96493932197dd5b658d32e294347
Jonas Dorfinger <contact@dorfingerjonas.at>
2025-04-23 15:48:36 +0200

    implement dashboard

--- client/src/app/views/dashboard/dashboard.component.ts
+++ client/src/app/views/dashboard/dashboard.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { DataService } from '../../services/data.service';
 import { HeaderService } from '../../services/header.service';
-import { Drink, Food, OrderableItem } from '../../models/models';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,52 +9,12 @@ import { Drink, Food, OrderableItem } from '../../models/models';
 })
 export class DashboardComponent {
 
-  public saleVolume: number;
-  private food: Food[];
-  private drinks: Drink[];
-
-  constructor(private readonly route: ActivatedRoute,
-              private readonly data: DataService,
+  constructor(private readonly data: DataService,
               private readonly header: HeaderService) {
-    this.food = [];
-    this.drinks = [];
 
     this.header.text = 'Dashboard';
 
     this.data.fetchData();
-
-    this.saleVolume = 0;
-
-    this.data.food.subscribe(f => this.food = f);
-    this.data.drinks.subscribe(d => this.drinks = d);
-
-    this.data.orders.subscribe(orders => {
-      const items: OrderableItem[] = [ ...this.food, ...this.drinks ];
-
-      if (!(orders && orders.length > 0)) return;
-
-      this.saleVolume = orders.map(o => {
-        const item: OrderableItem | undefined = items.find(i => i.id === o.itemId);
-
-        if (item) {
-          return {
-            id: item.id,
-            amount: o.amount,
-            name: item.name,
-            price: item.price
-          } as OrderableItem;
-        }
-
-        return {
-          id: -1,
-          amount: 0,
-          name: '',
-          price: 0
-        } as OrderableItem;
-      }).reduce((totalCost, item) => {
-        return totalCost + item.price * item.amount!;
-      }, 0);
-    });
   }
 
 }
