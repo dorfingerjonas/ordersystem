f6db92df8f977b05a4bdaf7d8db90837c3777535
Jonas Dorfinger <contact@dorfingerjonas.at>
2025-04-23 15:50:52 +0200

    update environment files

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,11 +1,4 @@
 export const environment = {
-  firebaseConfig: {
-    apiKey: 'AIzaSyAfbZejjjfNF-8A9mUqesRO9xLYGXSj7zo',
-    authDomain: 'order-sprinter.firebaseapp.com',
-    databaseURL: 'https://order-sprinter.firebaseio.com',
-    projectId: 'order-sprinter',
-    storageBucket: 'order-sprinter.appspot.com',
-    messagingSenderId: '295337076444',
-    appId: '1:295337076444:web:3e0c381bca0712be'
-  }
+  supabaseUrl: 'https://omnbbhkrulrzxpnqydug.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tbmJiaGtydWxyenhwbnF5ZHVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAxNTYzNTUsImV4cCI6MjA1NTczMjM1NX0.bMIv88mertQ9v1Ph4VnPfSrOS1-OMFK60LhAUE-nS84'
 };
