527a118c98b830a84e3fdf2491d97dff57e4a2ed
Jonas Dorfinger <contact@dorfingerjonas.at>
2025-11-15 11:57:52 +0100

    add dashboard statistics

--- client/src/app/views/dashboard/dashboard.component.ts
+++ client/src/app/views/dashboard/dashboard.component.ts
@@ -9,12 +9,36 @@ import { HeaderService } from '../../services/header.service';
 })
 export class DashboardComponent {
 
+  public revenuePaid: number;
+  public revenueUnPaid: number;
+  public revenue: number;
+
   constructor(private readonly data: DataService,
               private readonly header: HeaderService) {
-
     this.header.text = 'Dashboard';
 
+    this.revenuePaid = 0;
+    this.revenueUnPaid = 0;
+    this.revenue = 0;
+
     this.data.fetchData();
+
+    this.data.orderedItems.subscribe(items => {
+      this.revenuePaid = 0;
+      this.revenueUnPaid = 0;
+
+      if (!items || items.length === 0) return;
+
+      for (const item of items) {
+        if (item.paid) {
+          this.revenuePaid += item.item.price;
+        } else {
+          this.revenueUnPaid += item.item.price;
+        }
+      }
+
+      this.revenue = this.revenuePaid + this.revenueUnPaid;
+    });
   }
 
 }
